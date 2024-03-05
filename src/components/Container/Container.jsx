a8c6a7ef4da91d8adedcfad562c8cb5684b6dbbf
DmitryMuzalev <56193550+DmitryMuzalev@users.noreply.github.com>
2024-03-05 16:13:55 +0300

    init: Starting template.

---
+++ src/components/Container/Container.jsx
@@ -0,0 +1,6 @@
+import styles from "./Container.module.scss";
+
+function Container() {
+  return <div className={styles.container}>Container</div>;
+}
+export { Container };
---
+++ templates/default/TemplateName.jsx
@@ -0,0 +1,6 @@
+import styles from "./TemplateName.module.scss";
+
+function TemplateName() {
+  return <div className={styles.templateName}>TemplateName</div>;
+}
+export { TemplateName };
